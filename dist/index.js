0f0d7fa674dea3e2cdd1ae8bf4bd1dee9c638f21
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-04-06 10:04:56 +0200

    Add Js Code

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello");
+/*
+  Algorithms
+  - select item then toggle class active on click to icon-plus
+*/
+const items = document.querySelectorAll(".item");
+items.forEach((ele) => {
+    ele.addEventListener("click", () => {
+        ele.classList.toggle("active");
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+/*
+  Algorithms
+  - select item then toggle class active on click to icon-plus
+*/
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    ele.classList.toggle("active");
+  });
+});
